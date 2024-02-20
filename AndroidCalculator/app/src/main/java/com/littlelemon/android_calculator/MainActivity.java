f366329df33a029a071162d105966f7f0df845c6
SGDTNQZ <sagadat556@gmail.com>
2024-02-20 17:31:18 +0600

    m

--- AndroidCalculator/app/src/main/java/com/littlelemon/android_calculator/MainActivity.java
+++ AndroidCalculator/app/src/main/java/com/littlelemon/android_calculator/MainActivity.java
@@ -14,80 +14,80 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        answer_tv = findViewById(R.id.tv_answer);
-
-        button7 = findViewById(R.id.btn7);
-        button8 = findViewById(R.id.btn8);
-        button9 = findViewById(R.id.btn9);
-        button4 = findViewById(R.id.btn4);
-        button5 = findViewById(R.id.btn5);
-        button6 = findViewById(R.id.btn6);
-        button1 = findViewById(R.id.btn1);
-        button2 = findViewById(R.id.btn2);
-        button3 = findViewById(R.id.btn3);
-        button0 = findViewById(R.id.btn0);
-
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("7");
-            }
-        });
-        button8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("8");
-            }
-        });
-        button9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("9");
-            }
-        });
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("4");
-            }
-        });
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("5");
-            }
-        });
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("6");
-            }
-        });
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("1");
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("2");
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("3");
-            }
-        });
-        button0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                answer_tv.setText("0");
-            }
-        });
+//        answer_tv = findViewById(R.id.tv_answer);
+//
+//        button7 = findViewById(R.id.btn7);
+//        button8 = findViewById(R.id.btn8);
+//        button9 = findViewById(R.id.btn9);
+//        button4 = findViewById(R.id.btn4);
+//        button5 = findViewById(R.id.btn5);
+//        button6 = findViewById(R.id.btn6);
+//        button1 = findViewById(R.id.btn1);
+//        button2 = findViewById(R.id.btn2);
+//        button3 = findViewById(R.id.btn3);
+//        button0 = findViewById(R.id.btn0);
+//
+//        button7.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("7");
+//            }
+//        });
+//        button8.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("8");
+//            }
+//        });
+//        button9.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("9");
+//            }
+//        });
+//        button4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("4");
+//            }
+//        });
+//        button5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("5");
+//            }
+//        });
+//        button6.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("6");
+//            }
+//        });
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("1");
+//            }
+//        });
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("2");
+//            }
+//        });
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("3");
+//            }
+//        });
+//        button0.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                answer_tv.setText("0");
+//            }
+//        });
     }
 }
